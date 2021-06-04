e46a527dce86622d0f097a87b78302e00b69ef53
Kevin Liu <liuxiaoyang000514@gmail.com>
2021-06-04 14:07:21 -0400

    Finishes Half of Leader Animation
    Some animation might need to be revised.
    Also added plans for initial skill set.

--- Senren Banka Murasame/Data/SenrenBanka_Config.sql
+++ Senren Banka Murasame/Data/SenrenBanka_Config.sql
@@ -20,7 +20,7 @@ INSERT INTO Players(
 	/* CivName */ 'LOC_CIVILIZATION_SENREN_BANKA_NAME',
 	/* CivIcon */ 'ICON_CIVILIZATION_SENREN_BANKA',
 	/* UA Name */ 'LOC_TRAIT_CIVILIZATION_SENREN_BANKA_NAME',
-	/* UA Dscp */ 'LOC_TRAIT_CIVILIZATION_SENREN_BANKA_DESCRIPTION',
+	/* UA Dscp */ 'LOC_TRAIT_CIVILIZATION_SENREN_BANKA_DESCRIPTION_GS',
 	/* UA Icon */ 'ICON_CIVILIZATION_SENREN_BANKA',
 	/* LdrType */ 'LEADER_MURASAME',
 	/* LdrName */ 'LOC_LEADER_MURASAME_NAME',
@@ -37,9 +37,45 @@ VALUES (
 	/* Domain  */ 'Players:Expansion2_Players',
 	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
 	/* LdrType */ 'LEADER_MURASAME',
+	/* ItmType */ 'UNIT_JAPANESE_SAMURAI',
+	/* ItmIcon */ 'ICON_UNIT_JAPANESE_SAMURAI',
+	/* ItmName */ 'LOC_UNIT_JAPANESE_SAMURAI_NAME',
+	/* Dscrptn */ 'LOC_UNIT_JAPANESE_SAMURAI_DESCRIPTION',
+	/* SortIdx */ 10
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
+	/* LdrType */ 'LEADER_MURASAME',
+	/* ItmType */ 'DISTRICT_ONSEN_RESORT',
+	/* ItmIcon */ 'ICON_DISTRICT_ONSEN_RESORT',
+	/* ItmName */ 'LOC_DISTRICT_ONSEN_RESORT_NAME',
+	/* Dscrptn */ 'LOC_DISTRICT_ONSEN_RESORT_DESCRIPTION_GS',
+	/* SortIdx */ 20
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
+	/* LdrType */ 'LEADER_MURASAME',
 	/* ItmType */ 'BUILDING_KANMIDOKORO',
 	/* ItmIcon */ 'ICON_BUILDING_KANMIDOKORO',
 	/* ItmName */ 'LOC_BUILDING_KANMIDOKORO_NAME',
 	/* Dscrptn */ 'LOC_BUILDING_KANMIDOKORO_DESCRIPTION',
-	/* SortIdx */ 10
+	/* SortIdx */ 30
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
+	/* LdrType */ 'LEADER_MURASAME',
+	/* ItmType */ 'PROJECT_GOSHINTO_A',
+	/* ItmIcon */ 'ICON_PROJECT_GOSHINTO_A',
+	/* ItmName */ 'LOC_PROJECT_GOSHINTO_A_NAME_FRONTEND',
+	/* Dscrptn */ 'LOC_PROJECT_GOSHINTO_A_DESCRIPTION',
+	/* SortIdx */ 40
+),(
+	/* Domain  */ 'Players:Expansion2_Players',
+	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
+	/* LdrType */ 'LEADER_MURASAME',
+	/* ItmType */ 'PROJECT_GOSHINTO_B',
+	/* ItmIcon */ 'ICON_PROJECT_GOSHINTO_B',
+	/* ItmName */ 'LOC_PROJECT_GOSHINTO_B_NAME_FRONTEND',
+	/* Dscrptn */ 'LOC_PROJECT_GOSHINTO_B_DESCRIPTION',
+	/* SortIdx */ 50
 );
---
+++ Senren Banka Murasame/Geometries/Leader Murasame Shadowbox.geo
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<AssetObjects:GeometryInstance>
+<m_CookParams>
+<m_Values/>
+</m_CookParams>
+<m_Version>
+<major>0</major>
+<minor>0</minor>
+<build>0</build>
+<revision>0</revision>
+</m_Version>
+<m_Meshes>
+<Element>
+<m_Name text="pCubeShape1"/>
+<m_Groups>
+<Element>
+<m_Name text="lambert1"/>
+<m_nFirstPrim>0</m_nFirstPrim>
+<m_nPrims>12</m_nPrims>
+</Element>
+</m_Groups>
+<m_nBoundBoneCount>1</m_nBoundBoneCount>
+<m_nPrimitiveCount>12</m_nPrimitiveCount>
+<m_nVertexCount>14</m_nVertexCount>
+</Element>
+</m_Meshes>
+<m_Bones>
+<Element text="pCube1"/>
+<Element text="Bone"/>
+</m_Bones>
+<m_ModelName text="pCube1"/>
+<m_SourceFilePath text=""/>
+<m_SourceObjectName text=""/>
+<m_ImportedTime>0</m_ImportedTime>
+<m_ExportedTime>0</m_ExportedTime>
+<m_ClassName text="Leader_ShadowVolume"/>
+<m_DataFiles>
+<Element>
+<m_ID text="GR2"/>
+<m_RelativePath text="Leader Murasame Shadowbox.fgx"/>
+</Element>
+</m_DataFiles>
+<m_Name text="Leader Murasame Shadowbox"/>
+<m_Description text="Leader Murasame Shadowbox"/>
+<m_Tags>
+<Element text="Leader_ShadowVolume"/>
+</m_Tags>
+<m_Groups/>
+</AssetObjects:GeometryInstance>
