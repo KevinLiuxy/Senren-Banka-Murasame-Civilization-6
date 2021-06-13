47a0c2b3c1d2bbd9ff2f9aae3a4ac3c9e71d4029
Kevin Liu <liuxiaoyang000514@gmail.com>
2021-06-13 19:22:53 -0400

    Finishes Most Animations Except Defeat
    Included all model files and wwise project files into the repository except for source media files.

--- Senren Banka Murasame/Data/SenrenBanka_Config.sql
+++ Senren Banka Murasame/Data/SenrenBanka_Config.sql
@@ -15,22 +15,6 @@ INSERT INTO Players(
 	Portrait,
 	PortraitBackground
 ) VALUES (
-	/* Domain  */ 'Players:StandardPlayers',
-	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
-	/* CivName */ 'LOC_CIVILIZATION_SENREN_BANKA_NAME',
-	/* CivIcon */ 'ICON_CIVILIZATION_SENREN_BANKA',
-	/* UA Name */ 'LOC_TRAIT_CIVILIZATION_SENREN_BANKA_NAME',
-	/* UA Dscp */ 'LOC_TRAIT_CIVILIZATION_SENREN_BANKA_DESCRIPTION',
-	/* UA Icon */ 'ICON_CIVILIZATION_SENREN_BANKA',
-	/* LdrType */ 'LEADER_MURASAME',
-	/* LdrName */ 'LOC_LEADER_MURASAME_NAME',
-	/* LdrIcon */ 'ICON_LEADER_MURASAME',
-	/* LA Name */ 'LOC_TRAIT_LEADER_GUARDIAN_OF_HOORI_NAME',
-	/* LA Dscp */ 'LOC_TRAIT_LEADER_GUARDIAN_OF_HOORI_DESCRIPTION',
-	/* LA Icon */ 'ICON_LEADER_MURASAME',
-	/* Prtrait */ 'PORTRAIT_LEADER_GUARDIAN_OF_HOORI',
-	/* Backgnd */ 'PORTRAIT_BACKGROUND_SENREN_BANKA'
-),(
 	/* Domain  */ 'Players:Expansion1_Players',
 	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
 	/* CivName */ 'LOC_CIVILIZATION_SENREN_BANKA_NAME',
@@ -42,7 +26,7 @@ INSERT INTO Players(
 	/* LdrName */ 'LOC_LEADER_MURASAME_NAME',
 	/* LdrIcon */ 'ICON_LEADER_MURASAME',
 	/* LA Name */ 'LOC_TRAIT_LEADER_GUARDIAN_OF_HOORI_NAME',
-	/* LA Dscp */ 'LOC_TRAIT_LEADER_GUARDIAN_OF_HOORI_DESCRIPTION_RNF',
+	/* LA Dscp */ 'LOC_TRAIT_LEADER_GUARDIAN_OF_HOORI_DESCRIPTION',
 	/* LA Icon */ 'ICON_LEADER_MURASAME',
 	/* Prtrait */ 'PORTRAIT_LEADER_GUARDIAN_OF_HOORI',
 	/* Backgnd */ 'PORTRAIT_BACKGROUND_SENREN_BANKA'
@@ -66,33 +50,6 @@ INSERT INTO Players(
 
 INSERT INTO PlayerItems (Domain, CivilizationType, LeaderType, Type, Icon, Name, Description, SortIndex)
 VALUES (
-	/* Domain  */ 'Players:StandardPlayers',
-	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
-	/* LdrType */ 'LEADER_MURASAME',
-	/* ItmType */ 'UNIT_JAPANESE_SAMURAI',
-	/* ItmIcon */ 'ICON_UNIT_JAPANESE_SAMURAI',
-	/* ItmName */ 'LOC_UNIT_JAPANESE_SAMURAI_NAME',
-	/* Dscrptn */ 'LOC_UNIT_JAPANESE_SAMURAI_DESCRIPTION',
-	/* SortIdx */ 10
-),(
-	/* Domain  */ 'Players:StandardPlayers',
-	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
-	/* LdrType */ 'LEADER_MURASAME',
-	/* ItmType */ 'DISTRICT_ONSEN_TOWN',
-	/* ItmIcon */ 'ICON_DISTRICT_ONSEN_TOWN',
-	/* ItmName */ 'LOC_TRAIT_CIVILIZATION_DISTRICT_ONSEN_TOWN_NAME',
-	/* Dscrptn */ 'LOC_TRAIT_CIVILIZATION_DISTRICT_ONSEN_TOWN_DESCRIPTION',
-	/* SortIdx */ 20
-),(
-	/* Domain  */ 'Players:StandardPlayers',
-	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
-	/* LdrType */ 'LEADER_MURASAME',
-	/* ItmType */ 'IMPROVEMENT_KANMIDOKORO',
-	/* ItmIcon */ 'ICON_IMPROVEMENT_KANMIDOKORO',
-	/* ItmName */ 'LOC_TRAIT_CIVILIZATION_IMPROVEMENT_KANMIDOKORO_NAME',
-	/* Dscrptn */ 'LOC_TRAIT_CIVILIZATION_IMPROVEMENT_KANMIDOKORO_DESCRIPTION',
-	/* SortIdx */ 30
-),(
 	/* Domain  */ 'Players:Expansion1_Players',
 	/* CivType */ 'CIVILIZATION_SENREN_BANKA',
 	/* LdrType */ 'LEADER_MURASAME',
